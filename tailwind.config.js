c274a5d62fe918711c8eb895685bcd1684f41a55
pihda <62319714+pihdastudios@users.noreply.github.com>
2022-07-23 18:58:54 +0700

    reconvert to node exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
-export const content = ["./_includes/**/*.html", "./_layouts/**/*.html", "./blog/*.html", "./_posts/*.html", "./*.html"];
-export const theme = {
-  screens: {
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./_includes/**/*.html", "./_layouts/**/*.html", "./blog/*.html", "./_posts/*.html", "./*.html"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
+  plugins: [require("@tailwindcss/typography")],
 };
-export const plugins = [require("@tailwindcss/typography")];
+
