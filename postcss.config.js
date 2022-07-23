d0c554679e11db5aa2edcfb38d025276b7344858
Adib <3923297-igdoma@users.noreply.gitlab.com>
2022-07-23 17:28:27 +0700

    fix config

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  // plugins: [
-  //   require("postcss-import"),
-  //   require("tailwindcss"),
-  //   require("autoprefixer"),
-  //   require("postcss-nested"),
-  //   ...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
-  // ],
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-nested"),
+    ...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
+  ],
 };
